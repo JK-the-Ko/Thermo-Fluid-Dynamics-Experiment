211e05682efb1cf079cda7cf69dd8797237b618a
JK-the-Ko <rhworbs1124@naver.com>
2022-09-20 17:30:24 +0900

    first commit

---
+++ 2022-2/열유체공학실험_Week_4/module.py
@@ -0,0 +1,16 @@
+class calculator :
+  def __init__(self, num1, num2) :
+    self.num1 = num1
+    self.num2 = num2
+  
+  def add(self) :
+    return self.num1 + self.num2
+  
+  def subtract(self) :
+    return self.num1 - self.num2
+  
+  def multiply(self) :
+    return self.num1 * self.num2
+  
+  def divide(self) :
+    return self.num1 / self.num2
